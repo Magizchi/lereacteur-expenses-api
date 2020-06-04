331d0653a1200fcc927bbb90ecc4bb2334f99b57
Rajanan <g.rajan1993@gmail.com>
2020-06-04 13:30:04 +0200

    update var fr toi eng

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
-const cors = require('cors');
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const cors = require("cors");
 const app = express();
 
 app.use(cors());
 app.use(bodyParser.json());
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/Users', { useNewUrlParser: true });
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/Users", {
+  useNewUrlParser: true,
+});
 
 // const Users = mongoose.model('Users', {
 // 	user: { type: String, default: '' },
@@ -19,84 +21,79 @@ mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/Users', { useNe
 // 	montant: { type: Number, default: '' }
 // });
 
-const Users = mongoose.model('Users', {
-	user: { type: String, default: '' },
-	total: { type: Number, default: 0 },
-	expense: [
-		{
-			lieu: { type: String, default: '' },
-			montant: { type: Number, default: 0 }
-		}
-	]
-});
-// **Create USER**
-app.post('/create/user', async (req, res) => {
-	try {
-		const newUsers = new Users({
-			user: req.body.user
-		});
-		await newUsers.save();
-		res.json({ newUsers });
-	} catch (error) {
-		res.status(400).json({ error: error.message });
-	}
+const Users = mongoose.model("Users", {
+  user: { type: String, default: "" },
+  total: { type: Number, default: 0 },
+  expense: [
+    {
+      lieu: { type: String, default: "" },
+      montant: { type: Number, default: 0 },
+    },
+  ],
 });
 
 // **Read**
-app.get('/', async (req, res) => {
-	try {
-		const users = await Users.find();
-		res.json(users);
-	} catch (error) {
-		res.status(400).json({ error: error.message });
-	}
+app.get("/", async (req, res) => {
+  try {
+    const users = await Users.find();
+    res.json(users);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+});
+
+// **Create USER**
+app.post("/create/user", async (req, res) => {
+  try {
+    const newUsers = new Users({
+      user: req.body.user,
+    });
+    await newUsers.save();
+    res.json({ newUsers });
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
 });
 
 // **Update**
-app.post('/update', async (req, res) => {
-	console.log('req', req.body);
-
-	try {
-		if (req.body.choix && req.body.montant && req.body.lieu) {
-			console.log('la');
-			const user = await Users.findById(req.body.choix);
-			const add = {
-				montant: req.body.montant,
-				lieu: req.body.lieu
-			};
-			console.log('req', req.body.montant);
-
-			user.total = user.total + parseInt(req.body.montant);
-			console.log(user.total);
-
-			user.expense.push(add);
-			await user.save();
-			res.json({ user });
-		} else {
-			res.status(400).json({ message: 'Missing parameter' });
-		}
-	} catch (error) {
-		res.status(400).json({ error: error.message });
-	}
+app.post("/update", async (req, res) => {
+  const { selectedUser, amount, place } = req.body;
+  try {
+    if (selectedUser && amount && place) {
+      const user = await Users.findById(selectedUser);
+      const add = {
+        amount,
+        place,
+      };
+      user.total = user.total + parseInt(amount);
+      user.expense.push(add);
+      await user.save();
+      res.json({ user });
+    } else {
+      res.status(400).json({ message: "Missing parameter" });
+    }
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
 });
 
 // **Delete**
-app.post('/delete', async (req, res) => {
-	try {
-		if (req.body.id) {
-			const student = await Student.findOne({ _id: req.body.id });
-			// Autre manière de trouver un document à partir d'un `id` :
-			// const student = await Student.findById(req.body.id);
-			await student.remove();
-			res.json({ message: 'Removed' });
-		} else {
-			res.status(400).json({ message: 'Missing id' });
-		}
-	} catch (error) {
-		res.status(400).json({ error: error.message });
-	}
+app.post("/delete", async (req, res) => {
+  try {
+    if (req.body.id) {
+      const student = await Student.findOne({ _id: req.body.id });
+      // Autre manière de trouver un document à partir d'un `id` :
+      // const student = await Student.findById(req.body.id);
+      await student.remove();
+      res.json({ message: "Removed" });
+    } else {
+      res.status(400).json({ message: "Missing id" });
+    }
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
 });
 
 app.listen(process.env.PORT || 3600, () => {
-	console.log('Server started');
+  console.log("Server started");
 });
